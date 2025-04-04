f203cd848c186511c47d2cde75d104055261ab07
Gabriel Trindade <trindadi13@gmail.com>
2025-04-04 17:15:31 -0300

    typography component extends all props

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -1,53 +1,63 @@
+import React, { JSX } from "react";
+
+// Variantes da sua tipografia
+type TypographyVariants =
+  | "text-display-1"
+  | "text-display-2"
+  | "text-display-3"
+  | "text-heading-1"
+  | "text-heading-2"
+  | "text-heading-3"
+  | "text-heading-4"
+  | "text-button-l"
+  | "text-button-m"
+  | "text-button-s"
+  | "text-label-xxl"
+  | "text-label-xl"
+  | "text-label-lg"
+  | "text-label-md"
+  | "text-label-sm"
+  | "text-body-xxxl"
+  | "text-body-xxl-400"
+  | "text-body-xxl-500"
+  | "text-body-xxl-600"
+  | "text-body-xl-400"
+  | "text-body-xl-500"
+  | "text-body-xl-600"
+  | "text-body-lg-400"
+  | "text-body-lg-500"
+  | "text-body-lg-600"
+  | "text-body-md-400"
+  | "text-body-md-500"
+  | "text-body-md-600"
+  | "text-body-sm-400"
+  | "text-body-sm-500"
+  | "text-body-sm-600"
+  | "text-body-xs-400"
+  | "text-body-xs-500"
+  | "text-body-xs-600";
+
+type TypographyTag = keyof JSX.IntrinsicElements;
+
 type TypographyProps = {
-  as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "span" | "label";
-  variant:
-    | "text-display-1"
-    | "text-display-2"
-    | "text-display-3"
-    | "text-heading-1"
-    | "text-heading-2"
-    | "text-heading-3"
-    | "text-heading-4"
-    | "text-button-l"
-    | "text-button-m"
-    | "text-button-s"
-    | "text-label-xxl"
-    | "text-label-xl"
-    | "text-label-lg"
-    | "text-label-md"
-    | "text-label-sm"
-    | "text-body-xxxl"
-    | "text-body-xxl-400"
-    | "text-body-xxl-500"
-    | "text-body-xxl-600"
-    | "text-body-xl-400"
-    | "text-body-xl-500"
-    | "text-body-xl-600"
-    | "text-body-lg-400"
-    | "text-body-lg-500"
-    | "text-body-lg-600"
-    | "text-body-md-400"
-    | "text-body-md-500"
-    | "text-body-md-600"
-    | "text-body-sm-400"
-    | "text-body-sm-500"
-    | "text-body-sm-600"
-    | "text-body-xs-400"
-    | "text-body-xs-500"
-    | "text-body-xs-600";
+  as: TypographyTag;
+  variant: TypographyVariants;
   className?: string;
   children: React.ReactNode;
-};
+} & Omit<JSX.IntrinsicElements[TypographyTag], "className" | "children">;
 
-export const Typography = ({
-  as = "p",
+export const Typography =({
+  as,
   variant,
   className = "",
   children,
+  ...rest
 }: TypographyProps) => {
-  const Component = as;
+  const Component = (as || "p") as React.ElementType;
 
   return (
-    <Component className={`${variant} ${className}`}>{children}</Component>
+    <Component className={`${variant} ${className}`} {...rest}>
+      {children}
+    </Component>
   );
 };
