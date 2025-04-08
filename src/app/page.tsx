f8081be82f62b0357da7f89ebe81336b298f64d3
Gabriel Trindade <trindadi13@gmail.com>
2025-04-08 19:25:04 -0300

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
-import { Button } from "@/components/Button/Default/Button";
-import { ArrowLeft } from "@phosphor-icons/react";
+"use client";
+
+import { Input } from "@/components/Input/Default/Input";
+import { Chat } from "@phosphor-icons/react";
 
 export default function Home() {
   return (
     <div>
-      <Button
-        text="Button"
-        variant="tinted"
-        color="danger"
-        disabled
-        icon={<ArrowLeft />}
-        iconPosition="start"
-        onlyIcon
-      />
+      <Input placeholder="Enter text here" />
     </div>
   );
 }
