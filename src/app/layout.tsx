d4bdc075241591878565d8230d5031fa8d6efdc3
Rafael Lima <srafalimacosta@gmail.com>
2025-04-13 15:37:49 -0300

    fix: remove unnecessary color class from body in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} color- antialiased`}>
+      <body className={`${inter.variable} antialiased`}>
         <I18nProvider>{children}</I18nProvider>
       </body>
     </html>
