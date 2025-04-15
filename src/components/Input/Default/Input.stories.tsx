7205e2f6261b0e6d6ee6f84c73e507b81d8cafdc
Gabriel Trindade <trindadi13@gmail.com>
2025-04-15 19:01:32 -0300

    feat: add Input component with stories and tests

---
+++ src/components/Input/Default/Input.stories.tsx
@@ -0,0 +1,34 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Input } from "./Input";
+import { IconPhosphor } from "@/components/Icon/Icon";
+
+const meta: Meta<typeof Input> = {
+  title: "Components/Input",
+  component: Input,
+  argTypes: {
+    type: {
+      control: "select",
+      options: ["text", "email", "password", "number"],
+    },
+    status: {
+      control: "select",
+      options: ["success", "error", undefined],
+    },
+
+    placeholder: {
+      control: "text",
+    },
+  },
+  args: {
+    type: "text",
+    status: undefined,
+
+    icon: <IconPhosphor name="User" weight="fill" />,
+    rigthComponent: <IconPhosphor name="Acorn" />,
+
+    placeholder: "Placeholder",
+  },
+};
+export default meta;
+type Story = StoryObj<typeof Input>;
+export const Default: Story = {};
