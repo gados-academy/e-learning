63a5639b80d2ecbc77050e52f9091ee80c962a0d
Victor Hugo <vt.hugo.2021@gmail.com>
2025-04-15 18:49:55 -0300

    Select (#18)
    * feat: create initial select component
    
    * feat: focus on first element when open the select
    
    * feat: add keydown binds
    
    * --ammend
    
    * feat: update tabindex when moving the select item focus
    
    * refactor: add a11y to select component
    
    * refactor: add first test to select component
    
    * refactor: invert chrevon icon when open the select
    
    * test: add test for click and arrow down
    
    * test: add tests for arrow up
    
    * test: add tests for all other keybinds
    
    * test: add test when click again in select opener
    
    * refactor: remove unused comment
    
    * refactor: fix callouts
    
    * test: add missing test

---
+++ src/components/Select/Select.test.tsx
@@ -0,0 +1,419 @@
+import { cleanup, render, screen, waitFor } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+import { Select } from "./Select";
+import { SelectProps } from "./Select.types";
+import { afterEach, describe, expect, it, vi } from "vitest";
+import "@testing-library/jest-dom/vitest";
+
+const makeSut = ({
+  label = "click here",
+  items = [
+    { label: "item 1", value: "1" },
+    { label: "item 2", value: "2" },
+    { label: "item 3", value: "3" },
+  ],
+  onSelect = vi.fn(),
+  ...props
+}: Partial<SelectProps>) => {
+  render(<Select label={label} items={items} onSelect={onSelect} {...props} />);
+};
+
+describe("Select", () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  describe("when is mounted", () => {
+    it("renders the component", () => {
+      makeSut({});
+
+      const selectEl = screen.getByText("click here");
+      expect(selectEl).toBeInTheDocument();
+    });
+
+    it("has aria attributes", () => {
+      const label = "custom label";
+      makeSut({ label });
+
+      const buttonEl = screen.getByRole("button");
+      expect(buttonEl).toHaveAttribute("aria-label", label);
+      expect(buttonEl).toHaveAttribute("aria-expanded", "false");
+      expect(buttonEl).toHaveAttribute("aria-haspopup", "listbox");
+    });
+  });
+
+  describe("when click in the select button", () => {
+    it("opens the listbox", async () => {
+      makeSut({});
+
+      const listboxEl = screen.findByRole("listbox");
+      const buttonEl = screen.getByRole("button");
+      userEvent.click(buttonEl);
+
+      expect(await listboxEl).toBeInTheDocument();
+    });
+
+    it("updates open aria attribute", async () => {
+      makeSut({});
+
+      const listboxEl = screen.findByRole("listbox");
+      const buttonEl = screen.getByRole("button");
+      userEvent.click(buttonEl);
+
+      expect(await listboxEl).toBeInTheDocument();
+      expect(buttonEl).toHaveAttribute("aria-expanded", "true");
+    });
+
+    it("focus in the first item", async () => {
+      makeSut({});
+
+      const buttonEl = screen.getByRole("button");
+      userEvent.click(buttonEl);
+
+      await waitFor(async () => {
+        const firstItemEl = screen.getByRole("option", { name: /Item 1/i });
+
+        expect(firstItemEl).toHaveFocus();
+        expect(firstItemEl).toHaveAttribute("tabIndex", "0");
+      });
+    });
+
+    describe("and click again", () => {
+      it("closes the select", async () => {
+        makeSut({});
+
+        const buttonEl = screen.getByRole("button");
+        userEvent.click(buttonEl);
+
+        await waitFor(async () => {
+          const firstItemEl = screen.getByRole("option", { name: /Item 1/i });
+
+          expect(firstItemEl).toHaveFocus();
+          expect(firstItemEl).toHaveAttribute("tabIndex", "0");
+
+          await userEvent.click(buttonEl);
+
+          expect(firstItemEl).not.toBeInTheDocument();
+        });
+      });
+    });
+
+    describe("and press arrow down", () => {
+      it("focus in the second item", async () => {
+        makeSut({});
+        const userEvnt = userEvent.setup();
+
+        const buttonEl = screen.getByRole("button");
+        userEvnt.click(buttonEl);
+
+        await waitFor(async () => {
+          const firstItemEl = screen.getByRole("option", { name: /Item 1/i });
+          const secondItemEl = screen.getByRole("option", { name: /Item 2/i });
+
+          expect(firstItemEl).toHaveFocus();
+          expect(firstItemEl).toHaveAttribute("tabIndex", "0");
+
+          await userEvnt.keyboard("{ArrowDown}");
+
+          expect(secondItemEl).toHaveFocus();
+          expect(secondItemEl).toHaveAttribute("tabIndex", "0");
+
+          expect(firstItemEl).not.toHaveFocus();
+          expect(firstItemEl).toHaveAttribute("tabIndex", "-1");
+        });
+      });
+    });
+
+    describe("when press arrow down in the last item", () => {
+      it("focus on first element", async () => {
+        makeSut({});
+        const userEvnt = userEvent.setup();
+
+        const buttonEl = screen.getByRole("button");
+        userEvnt.click(buttonEl);
+
+        await waitFor(async () => {
+          const firstItemEl = screen.getByRole("option", { name: /Item 1/i });
+          const lastItemEl = screen.getByRole("option", {
+            name: /Item 3/i,
+          });
+
+          lastItemEl.focus();
+          expect(lastItemEl).toHaveFocus();
+
+          await userEvnt.keyboard("{ArrowDown}");
+
+          expect(firstItemEl).toHaveFocus();
+          expect(firstItemEl).toHaveAttribute("tabIndex", "0");
+
+          expect(lastItemEl).not.toHaveFocus();
+          expect(lastItemEl).toHaveAttribute("tabIndex", "-1");
+        });
+      });
+    });
+
+    describe("and press arrow up", () => {
+      it("focus in the up item", async () => {
+        makeSut({});
+        const userEvnt = userEvent.setup();
+
+        const buttonEl = screen.getByRole("button");
+        userEvnt.click(buttonEl);
+
+        await waitFor(async () => {
+          const secondItemEl = screen.getByRole("option", { name: /Item 2/i });
+          const firstItemEl = screen.getByRole("option", { name: /Item 1/i });
+
+          secondItemEl.focus();
+          expect(secondItemEl).toHaveFocus();
+
+          await userEvnt.keyboard("{ArrowUp}");
+
+          expect(firstItemEl).toHaveFocus();
+          expect(firstItemEl).toHaveAttribute("tabIndex", "0");
+
+          expect(secondItemEl).not.toHaveFocus();
+          expect(secondItemEl).toHaveAttribute("tabIndex", "-1");
+        });
+      });
+    });
+
+    describe("and press arrow up in the first item", () => {
+      it("focus in the last item", async () => {
+        makeSut({});
+        const userEvnt = userEvent.setup();
+
+        const buttonEl = screen.getByRole("button");
+        userEvnt.click(buttonEl);
+
+        await waitFor(async () => {
+          const firstItemEl = screen.getByRole("option", { name: /Item 1/i });
+          const lastItemEl = screen.getByRole("option", { name: /Item 3/i });
+
+          expect(firstItemEl).toHaveFocus();
+          expect(firstItemEl).toHaveAttribute("tabIndex", "0");
+
+          await userEvnt.keyboard("{ArrowUp}");
+
+          expect(lastItemEl).toHaveFocus();
+          expect(lastItemEl).toHaveAttribute("tabIndex", "0");
+
+          expect(firstItemEl).not.toHaveFocus();
+          expect(firstItemEl).toHaveAttribute("tabIndex", "-1");
+        });
+      });
+    });
+
+    describe("and press enter", () => {
+      it("calls the onSelect function", async () => {
+        const onSelectMock = vi.fn();
+
+        makeSut({ onSelect: onSelectMock });
+        const userEvnt = userEvent.setup();
+
+        const buttonEl = screen.getByRole("button");
+        userEvnt.click(buttonEl);
+
+        await waitFor(async () => {
+          const firstItemEl = screen.getByRole("option", { name: /Item 1/i });
+
+          expect(firstItemEl).toHaveFocus();
+          expect(firstItemEl).toHaveAttribute("tabIndex", "0");
+
+          await userEvnt.keyboard("{Enter}");
+
+          expect(onSelectMock).toHaveBeenCalledWith("1");
+        });
+      });
+    });
+
+    describe("and press Space", () => {
+      it("calls the onSelect function", async () => {
+        const onSelectMock = vi.fn();
+
+        makeSut({ onSelect: onSelectMock });
+        const userEvnt = userEvent.setup();
+
+        const buttonEl = screen.getByRole("button");
+        userEvnt.click(buttonEl);
+
+        await waitFor(async () => {
+          const firstItemEl = screen.getByRole("option", { name: /Item 1/i });
+
+          expect(firstItemEl).toHaveFocus();
+          expect(firstItemEl).toHaveAttribute("tabIndex", "0");
+
+          await userEvnt.keyboard("{Space}");
+
+          expect(onSelectMock).toHaveBeenCalledWith("1");
+        });
+      });
+    });
+
+    describe("and press Tab", () => {
+      it("calls the onSelect function", async () => {
+        const onSelectMock = vi.fn();
+
+        makeSut({ onSelect: onSelectMock });
+        const userEvnt = userEvent.setup();
+
+        const buttonEl = screen.getByRole("button");
+        userEvnt.click(buttonEl);
+
+        await waitFor(async () => {
+          const firstItemEl = screen.getByRole("option", { name: /Item 1/i });
+
+          expect(firstItemEl).toHaveFocus();
+          expect(firstItemEl).toHaveAttribute("tabIndex", "0");
+
+          await userEvnt.keyboard("{Tab}");
+
+          expect(onSelectMock).toHaveBeenCalledWith("1");
+        });
+      });
+    });
+
+    describe("and click in the item", () => {
+      it("calls the onSelect function", async () => {
+        const onSelectMock = vi.fn();
+
+        makeSut({ onSelect: onSelectMock });
+        const userEvnt = userEvent.setup();
+
+        const buttonEl = screen.getByRole("button");
+        userEvnt.click(buttonEl);
+
+        await waitFor(async () => {
+          const firstItemEl = screen.getByRole("option", { name: /Item 1/i });
+
+          expect(firstItemEl).toHaveFocus();
+          expect(firstItemEl).toHaveAttribute("tabIndex", "0");
+
+          await userEvnt.click(firstItemEl);
+
+          expect(onSelectMock).toHaveBeenCalledWith("1");
+        });
+      });
+
+      it("updates the aria-selected attribute", async () => {
+        makeSut({});
+        const userEvnt = userEvent.setup();
+
+        const buttonEl = screen.getByRole("button");
+
+        await userEvnt.click(buttonEl);
+
+        const firstItemEl = await screen.findByRole("option", {
+          name: /Item 1/i,
+        });
+        await userEvnt.click(firstItemEl);
+
+        await waitFor(() => {
+          expect(
+            screen.queryByRole("option", { name: /Item 1/i }),
+          ).not.toBeInTheDocument();
+        });
+
+        await userEvnt.click(buttonEl);
+
+        // a variavel `firstItemEl` perde a referencia do html porque o select se fecha depois de clicar, se usar a mesma variavel aqui o teste falha
+        expect(
+          await screen.findByRole("option", { name: /Item 1/i }),
+        ).toHaveAttribute("aria-selected", "true");
+      });
+    });
+
+    describe("and press Escape", () => {
+      it("closes the select", async () => {
+        makeSut({});
+        const userEvnt = userEvent.setup();
+
+        const buttonEl = screen.getByRole("button");
+        userEvnt.click(buttonEl);
+
+        await waitFor(async () => {
+          const firstItemEl = screen.queryByRole("option", { name: /Item 1/i });
+
+          expect(firstItemEl).toHaveFocus();
+          expect(firstItemEl).toHaveAttribute("tabIndex", "0");
+
+          await userEvnt.keyboard("{Escape}");
+
+          expect(firstItemEl).not.toBeInTheDocument();
+        });
+      });
+    });
+
+    describe("and press Backspace", () => {
+      it("closes the select", async () => {
+        makeSut({});
+        const userEvnt = userEvent.setup();
+
+        const buttonEl = screen.getByRole("button");
+        userEvnt.click(buttonEl);
+
+        await waitFor(async () => {
+          const firstItemEl = screen.queryByRole("option", { name: /Item 1/i });
+
+          expect(firstItemEl).toHaveFocus();
+          expect(firstItemEl).toHaveAttribute("tabIndex", "0");
+
+          await userEvnt.keyboard("{Backspace}");
+
+          expect(firstItemEl).not.toBeInTheDocument();
+        });
+      });
+    });
+
+    describe("and press Home", () => {
+      it("closes the select", async () => {
+        makeSut({});
+        const userEvnt = userEvent.setup();
+
+        const buttonEl = screen.getByRole("button");
+        userEvnt.click(buttonEl);
+
+        await waitFor(async () => {
+          const lastItemEl = screen.getByRole("option", { name: /Item 3/i });
+          const firstItemEl = screen.getByRole("option", { name: /Item 1/i });
+
+          lastItemEl.focus();
+          expect(lastItemEl).toHaveFocus();
+
+          await userEvnt.keyboard("{Home}");
+
+          expect(firstItemEl).toHaveFocus();
+          expect(firstItemEl).toHaveAttribute("tabIndex", "0");
+
+          expect(lastItemEl).not.toHaveFocus();
+          expect(lastItemEl).toHaveAttribute("tabIndex", "-1");
+        });
+      });
+    });
+
+    describe("and press End", () => {
+      it("closes the select", async () => {
+        makeSut({});
+        const userEvnt = userEvent.setup();
+
+        const buttonEl = screen.getByRole("button");
+        userEvnt.click(buttonEl);
+
+        await waitFor(async () => {
+          const lastItemEl = screen.getByRole("option", { name: /Item 3/i });
+          const firstItemEl = screen.getByRole("option", { name: /Item 1/i });
+
+          expect(firstItemEl).toHaveFocus();
+          expect(firstItemEl).toHaveAttribute("tabIndex", "0");
+          await userEvnt.keyboard("{End}");
+
+          expect(lastItemEl).toHaveFocus();
+          expect(lastItemEl).toHaveAttribute("tabIndex", "0");
+
+          expect(firstItemEl).not.toHaveFocus();
+          expect(firstItemEl).toHaveAttribute("tabIndex", "-1");
+        });
+      });
+    });
+  });
+});
---
+++ src/components/Select/Select.tsx
@@ -0,0 +1,180 @@
+import { useCallback, useRef, useState } from "react";
+import { SelectDirection, SelectProps } from "./Select.types";
+import { CaretDown } from "@phosphor-icons/react";
+import { Typography } from "@/components/Typography/Typography";
+import { Button } from "@/components/Button/Default/Button";
+
+export const Select = ({
+  label,
+  items,
+  defaultValue,
+  onSelect,
+  className,
+}: SelectProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(defaultValue ?? "");
+
+  const buttonRefs = useRef<Array<HTMLButtonElement | null>>([]);
+
+  const findFocusedIndex = () => {
+    const focusedElement = document.activeElement;
+    if (!focusedElement) {
+      return -1;
+    }
+
+    return buttonRefs.current.findIndex((ref) => ref === focusedElement);
+  };
+
+  const toggleOpen = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const handleSelect = (value: string) => {
+    const findLabel = items.find((item) => item.value === value)?.label;
+    if (!findLabel) return;
+
+    setSelectedItem(findLabel);
+    onSelect(value);
+    toggleOpen();
+  };
+
+  const updateButtonFocus = useCallback(
+    (nextIndex: number, direction: SelectDirection) => {
+      const lastIndex = buttonRefs.current.length - 1;
+      const increaseIndex = nextIndex > 0 ? nextIndex - 1 : lastIndex;
+      const decreaseIndex = nextIndex === lastIndex ? 0 : nextIndex + 1;
+
+      const indexDirection =
+        direction === "decreasing" ? decreaseIndex : increaseIndex;
+
+      buttonRefs.current[indexDirection]?.setAttribute("tabindex", "-1");
+      buttonRefs.current[nextIndex]?.setAttribute("tabindex", "0");
+      buttonRefs.current[nextIndex]?.focus();
+    },
+    [],
+  );
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLUListElement>) => {
+    if (!isOpen) return;
+
+    let currentFocusIndex = findFocusedIndex();
+    const lastIndex = buttonRefs.current.length - 1;
+
+    switch (event.key) {
+      case "ArrowDown":
+        event.preventDefault();
+
+        const nextIndex = ++currentFocusIndex;
+        const isLastElementFocused = lastIndex < nextIndex;
+
+        if (isLastElementFocused) {
+          updateButtonFocus(0, "increasing");
+          return;
+        }
+
+        updateButtonFocus(nextIndex, "increasing");
+        break;
+
+      case "ArrowUp":
+        event.preventDefault();
+        const prevIndex = --currentFocusIndex;
+        const isFirstElementFocused = prevIndex < 0;
+
+        if (isFirstElementFocused) {
+          updateButtonFocus(lastIndex, "decreasing");
+          return;
+        }
+
+        updateButtonFocus(prevIndex, "decreasing");
+        break;
+
+      case "Enter":
+      case " ":
+      case "Space":
+      case "Tab":
+        event.preventDefault();
+        if (currentFocusIndex !== -1 && items[currentFocusIndex]) {
+          handleSelect(items[currentFocusIndex].value);
+        }
+        break;
+      case "Escape":
+      case "Backspace":
+        event.preventDefault();
+        setIsOpen(false);
+        break;
+
+      case "Home":
+        updateButtonFocus(0, "decreasing");
+        break;
+
+      case "End":
+        updateButtonFocus(lastIndex, "decreasing");
+        break;
+    }
+  };
+
+  const assignRefAndFocusFirstRef = useCallback(
+    (node: HTMLButtonElement | null, index: number) => {
+      buttonRefs.current[index] = node;
+
+      if (node && index === 0) {
+        node.focus();
+      }
+    },
+    [],
+  );
+
+  const renderItems = () =>
+    items.map((item, index) => (
+      <li key={item.value} role="presentation" className="font-light">
+        <Button
+          tabIndex={index === 0 ? 0 : -1}
+          size="s"
+          role="option"
+          variant="ghost"
+          text={item.label}
+          aria-selected={item.label === selectedItem}
+          onClick={() => handleSelect(item.value)}
+          className="w-full !text-gray-700 justify-start"
+          ref={(node) => assignRefAndFocusFirstRef(node, index)}
+        />
+      </li>
+    ));
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label={label}
+          onClick={toggleOpen}
+          aria-expanded={isOpen}
+          aria-haspopup="listbox"
+          className={`flex cursor-pointer border-2 items-center px-4 py-3 border-gray-100 w-full justify-between ${className}`}
+        >
+          <Typography
+            tag="label"
+            variant="text-button-s"
+            className={selectedItem ? "text-gray-700" : "text-gray-500"}
+          >
+            {selectedItem || label}
+          </Typography>
+          <CaretDown
+            data-open={isOpen}
+            size={16}
+            className="transition-transform data-[open=true]:rotate-180"
+          />
+        </button>
+      </div>
+
+      {isOpen && (
+        <ul
+          role="listbox"
+          onKeyDown={handleKeyDown}
+          className="border-2 border-t-0 border-gray-100"
+        >
+          {renderItems()}
+        </ul>
+      )}
+    </div>
+  );
+};
